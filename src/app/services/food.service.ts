11bdd6efbef3f9d6633deb11db95547419589e41
teddy <hochart.dev@gmail.com>
2022-07-06 10:59:10 +0200

    add interfaces folder and food model / insert model in food.services

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,18 +1,19 @@
 /* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
+import { Food } from '../interfaces/food.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FoodService {
-  private _allFood = [];
+  private _allFood: Food[] = [];
 
   get allFood() {
     return this._allFood;
   }
   constructor() {}
 
-  addFood(foodItem) {
+  addFood(foodItem: Food) {
     this._allFood = [foodItem, ...this._allFood];
     console.log(this._allFood);
   }
