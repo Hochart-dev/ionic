660c097da9b864d8e22ac4de0cbeb9f95e0dd2e3
teddy <hochart.dev@gmail.com>
2022-07-07 09:58:05 +0200

    add firebase config in environment.ts  and install angular fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA2PFOX_iADBcMlTbpfNNI-cZtqMxnHJJM',
+    authDomain: 'freezer-manager-c31bb.firebaseapp.com',
+    projectId: 'freezer-manager-c31bb',
+    storageBucket: 'freezer-manager-c31bb.appspot.com',
+    messagingSenderId: '606155186768',
+    appId: '1:606155186768:web:9b4ea1483f5a6376372755',
+  },
 };
 
 /*
