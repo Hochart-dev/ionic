31a41fb5b8cfa90cbbf6648cbf6fc760ec65fb00
teddy <hochart.dev@gmail.com>
2022-07-25 11:34:01 +0200

    add ngOndestroy

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -1,20 +1,28 @@
 /* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { DocumentChangeAction } from '@angular/fire/compat/firestore';
+
 import { Food } from '../interfaces/food.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FoodService {
-  private _allFood: Food[] = [];
+  // private _allFood: Food[] = [];
 
-  get allFood() {
-    return this._allFood;
+  // get allFood() {
+  //   return this._allFood;
+  // }
+
+  constructor(private afs: AngularFirestore) {}
+
+  allFood() {
+    return this.afs.collection('freezer').snapshotChanges();
   }
-  constructor() {}
 
   addFood(foodItem: Food) {
-    this._allFood = [foodItem, ...this._allFood];
-    console.log(this._allFood);
+    // this._allFood = [foodItem, ...this._allFood];
+    // console.log(this._allFood);
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
+import { Food } from '../interfaces/food.model';
 import { FoodService } from '../services/food.service';
 
 @Component({
@@ -6,16 +8,33 @@ import { FoodService } from '../services/food.service';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page implements OnInit, OnDestroy {
   allFoodInFreezer = [];
+  sub: Subscription;
   constructor(private foodService: FoodService) {}
 
-  ngOnInit(): void {
-    this.allFoodInFreezer = this.foodService.allFood;
+  ngOnInit() {
+    // this.allFoodInFreezer = this.foodService.allFood;
+    this.sub = this.foodService.allFood().subscribe(
+      (data: any) =>
+        (this.allFoodInFreezer = data.map(
+          (e: any) =>
+            ({
+              id: e.payload.doc.id,
+              ...e.payload.doc.data(),
+            } as Food)
+        )),
+      (err) => {}
+    );
     console.log('ngOnInit', this.allFoodInFreezer);
   }
+
   ionViewWillEnter() {
-    this.allFoodInFreezer = this.foodService.allFood;
+    // this.allFoodInFreezer = this.foodService.allFood;
     console.log('IonViewWillEnter', this.allFoodInFreezer);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
